0c1f98b10eb9ac6a0d0b9980569926dd699bc263
Lardman <Lardman@DESKTOP-FQBNP2K>
2017-09-14 09:05:27 +0200

    Add project files.

---
+++ W3Ex3_SoapConsumerCalc/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace W3Ex3_SoapConsumerCalc
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+		}
+	}
+}
